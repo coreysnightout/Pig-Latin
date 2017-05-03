96589c0e37316e419209c3b2d53f7c92507f577e
corey redding and Justin Gomez <corey@>
2017-05-03 16:41:45 -0700

    restart jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,12 @@
+var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "x", "z", "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "X", "Z"];
+var qu = ["q", "u", "Q", "U"];
+var y = ["y", "Y"];
 
-
-var pigLatin = function(userInput) {
-
-  for(i = 0; i < userInput.length; i++) {
-
-    if(userInput[i] === "a" || userInput[i] === "e" || userInput[i] === "i" || userInput[i] === "o" || userInput[i] === "u" || userInput[i] === "A" || userInput[i] === "E" || userInput[i] === "I" || userInput[i] === "O" || userInput === "U") {
-
-      userInput = userInput.join("");
-      // return userInput + "way";
-      console.log(userInput);
-    }
-    // if(userInput[i] === "b" || userInput[i] === "c" || userInput[i] === "d" || userInput[i] === "f" || userInput[i] === "g" || userInput[i] === "h" || userInput[i] === "j" || userInput[i] === "k"                  || userInput[i] === "l" || userInput[i] === "m" || userInput[i] === "n" || userInput[i] === "p"  || userInput[i] === "q" || userInput[i] === "r" || userInput[i] === "s" || userInput[i] === "t" || userInput[i] === "v" || userInput[i] === "w"  || userInput[i] === "x" || userInput[i] === "y"
-    // || userInput[i] === "z") {
-    //
-    // }
-    }
-
-
-};
+var secondConsonant =
+var
 
 ////User Interface////
 $(document).ready(function() {
   $("#pig-latin").submit(function(event) {
     event.preventDefault();
-    var userInput = $("input#pigWord").val();
-    console.log(userInput);
-    userInput = userInput.split("");
-
-    // console.log(userInput);
-    var result = pigLatin(userInput);
-
-
-// userInput = userInput.join("");
-      //  console.log(userInput);
-
-
-    $("#result").text(userInput +"way");
-  });
-});
